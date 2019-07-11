d32fb9fb800d7dbeb3e3565f1ce74b73ea72dd45
ayankovsky <p44gr10@gmail.com>
2019-07-11 21:08:33 +0200

    fix(ci): fixed build

--- test/functional/basic/entity/Post.ts
+++ test/functional/basic/entity/Post.ts
@@ -1,6 +1,6 @@
-import { Column, Entity } from 'D:\\Development\\Workspace\\typeorm\\build\\package'
-import { PrimaryColumn } from 'D:\\Development\\Workspace\\typeorm\\build\\package'
-import { Generated } from 'D:\\Development\\Workspace\\typeorm\\build\\package'
+import { Column, Entity } from 'typeorm'
+import { PrimaryColumn } from 'typeorm'
+import { Generated } from 'typeorm'
 
 @Entity('aurora_data_api_test_post')
 export class Post {
