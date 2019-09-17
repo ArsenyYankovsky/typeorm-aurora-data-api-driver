f9957c60888b851c84db09ce6917c85f1f1c6daf
ayankovsky <p44gr10@gmail.com>
2019-09-17 23:33:21 +0200

    fix(log): removed extra parameter to fix logging

--- test/functional/basic/simple-queries.func.test.ts
+++ test/functional/basic/simple-queries.func.test.ts
@@ -28,7 +28,9 @@ describe('aurora data api > simple queries', () => {
 
     const result = await connection.query('select 1')
 
-    expect(logSpy).toHaveBeenCalledWith('select 12')
+    expect(logSpy).toHaveBeenCalledWith('query:')
+    expect(logSpy).toHaveBeenCalledWith('select 1')
+    expect(logSpy).toBeCalledTimes(2)
 
     expect(result[0][1]).toBe(1)
 
