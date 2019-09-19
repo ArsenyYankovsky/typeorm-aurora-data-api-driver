355a9ebe05bae0312260f73f2a1782ab10561df4
Mark Everett <mark@eighthdoorclub.com>
2019-09-19 21:29:22 +0100

    Test checkin

--- test/functional/utils/drop-all-tables.ts
+++ test/functional/utils/drop-all-tables.ts
@@ -2,11 +2,6 @@ import { Connection } from 'typeorm'
 
 export const dropAllTables = async (connection: Connection) => {
   await connection.synchronize(true)
-  /*
-  connection.entityMetadatas.forEach(
-    async entityMetadata => await connection.query(`DROP TABLE ${entityMetadata.tableName}`),
-  )
-  */
 }
 
 export const dropAllTablesAndCloseConnection = async (connection: Connection) => {
